94c217f215cb4871b683ac505d5af39ecfaa5377
José Eduardo Martins <m4rt1ns.jose@gmail.com>
2022-10-28 15:03:22 -0300

    update

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <input type="text" v-model="input">
+  <button>buscar</button>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      input: ''
+    }
+  },
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
